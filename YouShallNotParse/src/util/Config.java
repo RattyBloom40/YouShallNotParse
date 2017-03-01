71563096cb26d2f2bad79f4114fccdd421a3993a
Ethan B <e10benga@gmail.com>
2017-02-28 20:41:53 -0600

    Added Config class with item enum

---
+++ YouShallNotParse/src/util/Config.java
@@ -0,0 +1,14 @@
+package util;
+
+public class Config {
+	public static enum item {Empty,Torch,Shovel,Ladder,BlueKey,RedKey,GreenKey};
+	
+	public item slot1 = item.Empty;
+	public item slot2 = item.Empty;
+	public item slot3 = item.Empty;
+	public item slot4 = item.Empty;
+	public item slot5 = item.Empty;
+	public item slot6 = item.Empty;
+	
+	public int slots = 0;
+}
